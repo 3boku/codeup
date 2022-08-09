206da420d7b191f65baa47b51b8ae3e11edc5213
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2022-08-09 23:14:57 +0900

    Add files via upload

---
+++ 1061.cpp
@@ -0,0 +1,15 @@
+//
+//  1061.cpp
+//  C
+//
+//  Created by Hyun Seo Jung on 2022/08/07.
+//
+
+#include "1061.hpp"
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    scanf(" %d %d", &a, &b);
+    printf("%d", a|b);
+}
